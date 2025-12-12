f24a12812d9f7e15517eda84e04e67f8e7cee400
gabrielfiskbaek <gar001@edu.zealand.dk>
2025-12-12 12:13:34 +0100

    routes done

---
+++ routes/budgetRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const budgetController = require('../controller/budgetController');
+
+const router = express.Router();
+
+// Hent alle budgetter
+router.get('/', budgetController.getAllBudgets);
+// Opret et nyt budget
+router.post('/', budgetController.createBudget);
+
+module.exports = router;
---
+++ routes/transactionRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const transactionController = require('../controller/transactionController');
+
+const router = express.Router();
+
+//hent alle transaktioner
+router.get('/', transactionController.getAllTransactions);
+//hent en enkelt transaktion ved ID
+router.get('/:id', transactionController.getTransactionById);
+//opret en ny transaktion
+router.post('/', transactionController.createTransaction);
+//opdater en transaktion ved ID
+router.put('/:id', transactionController.updateTransaction);
+//Slet en transaktion ved ID
+router.delete('/:id', transactionController.deleteTransaction);
+
+module.exports = router;
