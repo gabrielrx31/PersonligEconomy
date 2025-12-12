229ed1302d8f561880e24d683cf75f0e24d48e56
gabrielfiskbaek <gar001@edu.zealand.dk>
2025-12-12 12:13:51 +0100

    i gang med app.js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const app = express();
-const port = 3000;
+const mongoose = require('mongoose');
+const logger = require('./middleware/logger');
+const validateTransaction = require('./middleware/validateTransaction');
+const validateBudget = require('./middleware/validateBudget');
 
+const app = express();
+
+app.use(express.json());
+app.use(logger);
+
+mongoose.connect('mongodb://localhost:27017/personal_finance')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+    
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
